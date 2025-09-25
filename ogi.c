20630fee1ace27151d16ad53109d2ed5a1c56d00
burakyaslan <burak.yaslan@octoxlabs.com>
2025-09-25 22:09:21 +0300

    new c file

---
+++ ogi.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Includes the standard input/output library
+
+int main() { // The main function where program execution begins
+    printf("Hello, World!\n"); // Prints the string "Hello, World!" followed by a new line
+    return 0; // Indicates successful program termination
+}
